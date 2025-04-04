bdc7b8b531c770d4e7d0f97cf112de8cbdf9fb89
MohamedIbnKhaled <mohamedrobot1@gmail.com>
2025-04-04 08:42:14 +0200

    first commit

---
+++ MovieHub-backend/src/main/java/com/Fawry/MovieHub_backend/MovieHubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Fawry.MovieHub_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieHubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieHubBackendApplication.class, args);
+	}
+
+}
---
+++ MovieHub-backend/src/test/java/com/Fawry/MovieHub_backend/MovieHubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Fawry.MovieHub_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieHubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
